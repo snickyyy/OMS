df682105995ecc9d7ba96e16390248f75541f736
sc.snicky <leon12345671234@gmail.com>
2026-02-03 17:11:41 +0100

    feat: add CustomerId value object and integrate it into Order entity

--- src/main/java/sc/snicky/oms/domain/models/Order.java
+++ src/main/java/sc/snicky/oms/domain/models/Order.java
@@ -2,6 +2,7 @@ package sc.snicky.oms.domain.models;
 
 import lombok.Builder;
 import lombok.Getter;
+import sc.snicky.oms.domain.models.values.order.CustomerId;
 import sc.snicky.oms.domain.models.values.order.OrderId;
 import sc.snicky.oms.domain.models.values.order.OrderNumber;
 import sc.snicky.oms.domain.models.values.order.OrderStatus;
@@ -11,7 +12,7 @@ import sc.snicky.oms.domain.models.values.order.OrderStatus;
 public class Order {
     private final OrderId id;
     private final OrderNumber orderNumber;
-
+    private final CustomerId customerId;
     private OrderStatus orderStatus;
 
 
---
+++ src/main/java/sc/snicky/oms/domain/models/values/order/CustomerId.java
@@ -0,0 +1,29 @@
+package sc.snicky.oms.domain.models.values.order;
+
+import sc.snicky.oms.domain.exceptions.ValidationRuleException;
+import sc.snicky.oms.domain.exceptions.codes.OrderErrorCode;
+import sc.snicky.oms.domain.models.values.BaseValueObject;
+
+public class CustomerId extends BaseValueObject<String> {
+    private static final int MAX_LENGTH = 255;
+
+    public CustomerId(String value) {
+        super(value);
+    }
+
+    @Override
+    public void validate() {
+        if (getValue() == null || getValue().isEmpty()) {
+            throw new ValidationRuleException(
+                    "Customer ID cannot be null or empty",
+                    OrderErrorCode.CUSTOMER_ID_EMPTY_OR_NULL.name()
+            );
+        }
+        if (getValue().length() > MAX_LENGTH) {
+            throw new ValidationRuleException(
+                    "Customer ID cannot exceed " + MAX_LENGTH + " characters",
+                    OrderErrorCode.CUSTOMER_ID_INVALID_LENGTH.name()
+            );
+        }
+    }
+}
