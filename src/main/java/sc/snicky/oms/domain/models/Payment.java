5a1f8e3986b44c743ee47e0e0f16bc9277b8ca46
sc.snicky <leon12345671234@gmail.com>
2026-02-05 12:38:32 +0100

    feat: enhance Payment entity with amount and method fields, and add PaymentMethod enum

--- src/main/java/sc/snicky/oms/domain/models/Payment.java
+++ src/main/java/sc/snicky/oms/domain/models/Payment.java
@@ -2,8 +2,10 @@ package sc.snicky.oms.domain.models;
 
 import lombok.Builder;
 import lombok.Getter;
+import sc.snicky.oms.domain.models.values.Money;
 import sc.snicky.oms.domain.models.values.order.OrderId;
 import sc.snicky.oms.domain.models.values.payments.PaymentId;
+import sc.snicky.oms.domain.models.values.payments.PaymentMethod;
 import sc.snicky.oms.domain.models.values.payments.PaymentStatus;
 import sc.snicky.oms.domain.models.values.payments.TransactionId;
 
@@ -14,13 +16,17 @@ public class Payment {
     private final PaymentId id;
     private final TransactionId transactionId;
     private final OrderId orderId;
+    private final Money amount;
+    private final PaymentMethod method;
     private PaymentStatus status;
 
     @Builder
-    public Payment(PaymentId id, TransactionId transactionId, OrderId orderId, PaymentStatus status) {
+    public Payment(PaymentId id, TransactionId transactionId, OrderId orderId, PaymentStatus status, Money amount, PaymentMethod method) {
         this.id = Objects.requireNonNull(id);
         this.transactionId = Objects.requireNonNull(transactionId);
         this.orderId = Objects.requireNonNull(orderId);
         this.status = Objects.requireNonNull(status);
+        this.amount = Objects.requireNonNull(amount);
+        this.method = Objects.requireNonNull(method);
     }
 }
---
+++ src/main/java/sc/snicky/oms/domain/models/values/payments/PaymentMethod.java
@@ -0,0 +1,8 @@
+package sc.snicky.oms.domain.models.values.payments;
+
+public enum PaymentMethod {
+    CREDIT_CARD,
+    DEBIT_CARD,
+    BANK_TRANSFER,
+    CASH_ON_DELIVERY
+}
