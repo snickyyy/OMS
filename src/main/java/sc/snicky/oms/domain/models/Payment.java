fd251eac2e159b6dd2af193d1122ceb5979b6ad2
sc.snicky <leon12345671234@gmail.com>
2026-02-05 12:34:51 +0100

    feat: add PaymentStatus enum and update Payment entity to include orderId and status management

--- src/main/java/sc/snicky/oms/domain/models/Payment.java
+++ src/main/java/sc/snicky/oms/domain/models/Payment.java
@@ -2,7 +2,9 @@ package sc.snicky.oms.domain.models;
 
 import lombok.Builder;
 import lombok.Getter;
+import sc.snicky.oms.domain.models.values.order.OrderId;
 import sc.snicky.oms.domain.models.values.payments.PaymentId;
+import sc.snicky.oms.domain.models.values.payments.PaymentStatus;
 import sc.snicky.oms.domain.models.values.payments.TransactionId;
 
 import java.util.Objects;
@@ -11,11 +13,14 @@ import java.util.Objects;
 public class Payment {
     private final PaymentId id;
     private final TransactionId transactionId;
+    private final OrderId orderId;
+    private PaymentStatus status;
 
     @Builder
-    public Payment(PaymentId id, TransactionId transactionId) {
+    public Payment(PaymentId id, TransactionId transactionId, OrderId orderId, PaymentStatus status) {
         this.id = Objects.requireNonNull(id);
         this.transactionId = Objects.requireNonNull(transactionId);
-        
+        this.orderId = Objects.requireNonNull(orderId);
+        this.status = Objects.requireNonNull(status);
     }
 }
