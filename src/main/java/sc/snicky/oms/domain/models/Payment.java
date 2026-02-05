8b5823112f300a235cb71f44f961db440df33d12
sc.snicky <leon12345671234@gmail.com>
2026-02-05 12:28:01 +0100

    feat: refactor Payment entity to use value objects for identifiers and add TransactionId value object

--- src/main/java/sc/snicky/oms/domain/models/Payment.java
+++ src/main/java/sc/snicky/oms/domain/models/Payment.java
@@ -2,13 +2,20 @@ package sc.snicky.oms.domain.models;
 
 import lombok.Builder;
 import lombok.Getter;
+import sc.snicky.oms.domain.models.values.payments.PaymentId;
+import sc.snicky.oms.domain.models.values.payments.TransactionId;
+
+import java.util.Objects;
 
 @Getter
 public class Payment {
-    private final String id;
+    private final PaymentId id;
+    private final TransactionId transactionId;
 
     @Builder
-    public Payment(String id) {
-        this.id = id;
+    public Payment(PaymentId id, TransactionId transactionId) {
+        this.id = Objects.requireNonNull(id);
+        this.transactionId = Objects.requireNonNull(transactionId);
+        
     }
 }
